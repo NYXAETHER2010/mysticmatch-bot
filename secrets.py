d9da995a3cf9f05de0825cac4410c7cd0be52207
NYXAETHER2010 <sanjusuman3415@gmail.com>
2025-11-30 19:26:44 +0530

    Add secrets.py for environment variables

---
+++ secrets.py
@@ -0,0 +1,5 @@
+import os
+
+# Your secret tokens
+TELEGRAM_BOT_TOKEN = os.getenv("TELEGRAM_BOT_TOKEN", "8559349902:AAHlznRzC8X3s5CQnggaD7DOUAHi-UjXktU")
+MONGODB_URI = os.getenv("MONGODB_URI", "mongodb+srv://mysticuser:Match2025@cluster0.ugfuqiq.mongodb.net/?appName=Cluster0")
